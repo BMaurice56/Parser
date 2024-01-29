ff3b960b08e4ffe5bcc272127e02ee21bfe4a65c
Benoit <benoitmaurice56@protonmail.com>
2024-01-29 14:41:00 +0100

    Ajout lecture et création fichier pdf

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import os
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+nomFichierTexte = "fichier.txt"
 
 
-# Press the green button in the gutter to run the script.
+def checkPDFFile(nomFichier: str) -> None:
+    """
+    Vérifie si le nom de fichier fourni est bien un pdf
+    """
+    if not os.path.exists(nomFichier):
+        raise FileNotFoundError("Le fichier n'existe pas ")
+
+    if not os.path.isfile(nomFichier) or nomFichier[-4:] != ".pdf":
+        raise FileExistsError(f"{nomFichier} n'est pas un fichier .pdf")
+
+
+def createTxtFileFromPdf(nomFichier: str) -> None:
+    """
+    Crée le fichier texte depuis le fichier pdf
+    nomFichier : nom du fichier
+
+    Nom du fichier de sortie → fichier.txt
+    """
+    checkPDFFile(nomFichier)
+
+    if os.system(f"pdftotext -raw {nomFichier} {nomFichierTexte}") != 0:
+        raise OSError("Impossible de transformer le fichier pdf en texte")
+
+
+def openTXTFile() -> str:
+    """
+    Ouvre le fichier txt et renvoie son contenu sans caractère inconnu
+    :return: Contenu du fichier
+    """
+    with open(nomFichierTexte, "f") as f:
+        content = f.read()
+
+    return content
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    file = "/home/benoit/Documents/cours/Parser/Corpus_2022/Boudin-Torres-2006.pdf"
+    createTxtFileFromPdf(file)
