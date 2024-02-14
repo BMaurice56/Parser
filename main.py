982c92970d648f386bfba1a35afe5aa324dce7c5
Benoit <benoitmaurice56@protonmail.com>
2024-02-14 14:20:31 +0100

    Modification récupération abstract + test autre méthode récupération auteurs

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import os
 
 
 class Parser:
+    pdfReader = PyPDF2.PdfReader
     pathToFile = ""
     nomFichier = ""
     directoryTxtFile = ""
-    pdfReader = PyPDF2.PdfReader
     titre = ""
+    auteurs = ""
+    abstract = ""
+    numbers_last_char = previous_number_last_char = 0
 
     def __init__(self, path: str, nomFichier: str, directoryTxtFile: str = None):
         self.pathToFile = path
         self.nomFichier = nomFichier
 
-        if not self.isPDFFile(self.pathToFile + self.nomFichier):
+        if not self.isPDFFile(path + nomFichier):
+            print(f"Nom du fichier : {nomFichier}")
             raise FileNotFoundError("Le fichier fourni n'est pas un pdf")
 
         self.pdfReader = self.openPDF()
@@ -46,13 +50,38 @@ class Parser:
 
         return PyPDF2.PdfReader(pdfFileObj)
 
-    def getAuthor(self, minimum_y=600, maximum_y=750) -> str | list:
+    def getAuthor(self) -> str:
         """
         Renvoi la liste des auteurs
 
         :return: List des auteurs
         """
+        metadata = self.pdfReader.metadata.author
 
+        if metadata is not None and metadata != "" and "@" not in metadata:
+            return metadata
+
+        page = self.pdfReader.pages[0].extract_text()
+
+        self.getTitle()
+        self.getAbstract()
+
+        pos_titre = page.find(self.titre)
+        pos_abstract = page.find(self.abstract)
+
+        self.auteurs = page[pos_titre + len(self.titre): pos_abstract]
+
+        if "Abstract" in self.auteurs.strip():
+            self.auteurs = self.auteurs[:self.auteurs.find("Abstract") - 1]
+
+        return self.auteurs
+
+    def old_getAuthor(self, minimum_y=530, maximum_y=720, last_char=150) -> str:
+        """
+        Renvoi la liste des auteurs
+
+        :return: List des auteurs
+        """
         metadata = self.pdfReader.metadata.author
 
         if metadata is not None and metadata != "" and "@" not in metadata:
@@ -64,32 +93,86 @@ class Parser:
 
         def visitor_body(text, cm, tm, fontDict, fontSize):
             if text != "" and text != " " and text != "\n":
-                # print(f"texte : {text}")
-                # print(f"cm : {cm}")
-                # print(f"tm : {tm}")
-                # print(f"fontDict : {fontDict}")
-                # print(f"fontSize : {fontSize}")
+
                 y = tm[5]
-                if minimum_y < y < maximum_y:
+
+                # Permet de vérifier si la valeur n'est pas trop basse
+                if "/LastChar" in fontDict:
+                    if self.previous_number_last_char != fontDict["/LastChar"]:
+                        if self.numbers_last_char < 0:
+                            self.numbers_last_char = -float("inf")
+                        else:
+                            self.numbers_last_char = 0
+
+                        self.previous_number_last_char = fontDict["/LastChar"]
+                    else:
+                        self.numbers_last_char += 1
+
+                    if self.numbers_last_char > 15:
+                        raise Exception()
+
+                if minimum_y < y < maximum_y and fontDict["/LastChar"] <= last_char:
                     parts.append(text)
 
-        page.extract_text(visitor_text=visitor_body)
+        try:
+            page.extract_text(visitor_text=visitor_body)
 
-        if len(page) == 0:
-            parts = self.getAuthor(minimum_y - 100, maximum_y + 50)
+        except:
+            parts.clear()
+            last_char = self.previous_number_last_char
+            self.numbers_last_char = -float("inf")
+            page = self.pdfReader.pages[0]
 
-        return parts
+            page.extract_text(visitor_text=visitor_body)
+
+        # content = page.extract_text()
+
+        # print(content)
+        print(parts)
+
+        # Récupère le titre
+        if self.titre == "" or self.titre is None:
+            self.titre = self.getTitle()
+
+        # Si le titre est présent dans le texte récupéré, on l'enlève
+        for elt in parts:
+            if elt.strip() in self.titre.strip() or "@" in elt:
+                # Si des éléments se trouvent devant le titre, on les enlève
+                indice = parts.index(elt)
+                if indice != 0:
+                    for i in range(indice):
+                        parts.pop(0)
+
+                parts.remove(elt)
+
+        print(parts)
+        # Si aucun élément n'a été récupérer, on augmente la fenêtre
+        if len(parts) == 0:
+            self.auteurs = self.old_getAuthor(minimum_y - 10, maximum_y + 10, last_char + 20)
+
+        else:
+            for elt2 in parts:
+                if "∗" in elt2:
+                    break
+                if "\n" in elt2:
+                    elt2 = elt2[:-1]
+                self.auteurs += elt2 + " ; "
+
+            self.auteurs = self.auteurs[:-2]
+
+        return self.auteurs
 
     def getTitle(self, minimum_y=600) -> str | None:
         """
         Renvoie le titre du pdf
 
+        :param minimum_y position minimal en y
         :return: Titre
         """
         # Vérifie les métadonnées
         self.titre = self.pdfReader.metadata.title
 
-        if self.titre is None:
+        if self.titre is None or "/" in self.titre:
             self.titre = ""
 
             page = self.pdfReader.pages[0]
@@ -99,7 +182,7 @@ class Parser:
             def visitor_body(text, cm, tm, fontDict, fontSize):
                 if text != "" and text != " " and text != "\n":
                     y = tm[5]
-                    if minimum_y < y < 750:
+                    if minimum_y < y < 700:
                         parts.append(text)
 
             # Extraction des premières lignes
@@ -112,18 +195,23 @@ class Parser:
                     i += 1
 
                 self.titre += parts[i]
+
+                # Si on n'a pas récupéré la deuxième ligne du titre, on augmente la fenêtre
+                if self.titre[-1] == "\n":
+                    self.titre = self.getTitle(minimum_y - 10)
+
             else:
-                self.titre = self.getTitle(minimum_y - 25)
+                self.titre = self.getTitle(minimum_y - 10)
 
             return self.titre
 
         return self.titre
 
-    def getAbstract(self) -> str | None:
+    def getAbstract(self) -> str:
         """
         Renvoie l'abstract du pdf
 
-        :return: String ou None si non trouvée
+        :return: String
         """
         numero_page = 0
         number_of_pages = len(self.pdfReader.pages)
@@ -141,22 +229,30 @@ class Parser:
 
             # Si trouvé, alors on peut renvoyer l'abstract
             if pos_abstract != -1 and pos_introduction != -1:
-                return content[pos_abstract + len("Abstract") + 1:pos_introduction - 2]
+                swift = 1
+                if content[pos_abstract + len("Abstract") + swift] in [" ", "\n"]:
+                    swift += 1
+
+                self.abstract = content[pos_abstract + len("Abstract") + swift:pos_introduction - 2]
+                break
 
             # Sinon absence du mot abstract
             elif pos_abstract == -1 and pos_introduction != -1:
-
-                dernier_point = content[:pos_introduction].rfind(".")
+                dernier_point = content[:pos_introduction - 2].rfind(".")
 
                 i = 0
+
                 for i in range(dernier_point, 1, -1):
                     if ord(content[i]) < 20:
                         if ord(content[i - 1]) != 45:
                             break
 
-                return content[i + 1:dernier_point]
+                self.abstract = content[i + 1:dernier_point]
+                break
 
             numero_page += 1
+
+        return self.abstract
 
     def writeValueInFile(self) -> None:
         """
