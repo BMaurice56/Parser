a2e0b9c05482c6734834dd88f2b93221f3ea0b45
arthur <arthurgros03@gmail.com>
2024-02-20 13:07:06 +0100

    actu_test

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import os
 import unittest
-
 import re
 
 def isTXT(nomFichier : str) ->bool:
@@ -10,12 +9,12 @@ def isTXTFiles(path: str) -> bool:
     """
     Vérifie si il y a que des txt dans le dossier fourni
 
-    :param nom du dossier
+    :param path du dossier
     :return: True ou False
     """
 
     for element in os.listdir(path):
-        if not isTXT(path + element):
+        if isTXT(path + element):
             return False
     return True
 
@@ -67,24 +66,131 @@ def read_text_file(file_path):
         return f"Une erreur s'est produite lors de la lecture du fichier : {str(e)}"
 
 
-information = extract_information("/home/arthur/Documents/L3/semestre_4/Parser/Corpus_2022/analyse_pdf/Boudin-Torres-2006.txt")
+class TestComparison(unittest.TestCase):
 
-for key, value in information.items():
-    print(f"{key} :\n    {value}")
-
-
-class TestDictionaryComparison(unittest.TestCase):
+    path_d="/home/arthur/Documents/L3/semestre_4/Parser/Corpus_2022/analyse_pdf"
+    def test_isTxt(self):
+        self.assertEqual(isTXTFiles(self.path_d),True)
 
     def test_Boudin(self):
         titre = "A Scalable MMR Approach to Sentence Scoring for Multi-Document Update Summarization"
         Nom = "Boudin-Torres-2006.pdf"
-        auteur = "Florian Boudin ; Marc El-Beze ; Juan-Manuel Torres-Moreno"
-        result = extract_information("/home/arthur/Documents/L3/semestre_4/Parser/Corpus_2022/analyse_pdf/Boudin-Torres-2006.txt")
+        auteur = "" #TO DO
+        abstract = "We present S MMR , a scalable sentence ..."
 
-        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre , "Auteurs": auteur,"Abstract":"We present S MMR , a scalable sentence ..."}
+        result = extract_information(self.path_d+"/Boudin-Torres-2006.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre , "Auteurs": auteur,"Abstract":abstract}
+
         self.assertEqual(result, dict2)
 
-    def test_dictionaries_not_equal(self):
-        dict1 = {'a': 1, 'b': 2, 'c': 3}
-        dict2 = {'a': 1, 'b': 2, 'c': 4}
-        self.assertNotEqual(dict1, dict2)
+
+    def test_Das_Martin(self):
+        titre = "Das_Martins.pdf"
+        Nom = "A Survey on Automatic Text Summarization"
+        auteur = ""
+        abstract = "The increasing availability of online information ..."
+        result = extract_information(self.path_d+"/Das_Martins.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
+
+    def test_Gonzalez_2018_Wisebe(self):
+        titre = "Gonzalez_2018_Wisebe.pdf"
+        Nom = "WiSeBE: Window-Based Sentence Boundary Evaluation"
+        auteur = ""
+        abstract = "Sentence Boundary Detection (SBD) has been a ..."
+        result = extract_information(self.path_d+"/Gonzalez_2018_Wisebe.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
+
+    def test_Iria_Juan_Manuel_Gerardo(self):
+        titre = "Iria_Juan-Manuel_Gerardo.pdf"
+        Nom = "On the Development of the RST Spanish Treebank"
+        auteur = ""
+        abstract = "In this article we present the RST Spanish  ..."
+        result = extract_information(self.path_d+"/Iria_Juan-Manuel_Gerardo.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
+
+    def test_jing_cutepaste(self):
+        titre = "jing-cutepaste.pdf"
+        Nom = "Cut and Paste Based Text Summarization"
+        auteur = ""
+        abstract = "We present a cut and paste based text summa-  ..."
+        result = extract_information(self.path_d+"/jing-cutepaste.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
+
+    def kessler94715(self):
+        titre = ""
+        Nom = ""
+        auteur = ""
+        abstract = ""
+        result = extract_information(self.path_d+"/kessler94715.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
+
+    def kesslerMETICS_ICDIM2019(self):
+        titre = ""
+        Nom = ""
+        auteur = ""
+        abstract = ""
+        result = extract_information(self.path_d+"/kesslerMETICS-ICDIM2019.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
+
+    def mikheev_J02_3002(self):
+        titre = ""
+        Nom = ""
+        auteur = ""
+        abstract = ""
+        result = extract_information(self.path_d+"/mikheev J02-3002.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
+
+    def Mikolov(self):
+        titre = ""
+        Nom = ""
+        auteur = ""
+        abstract = ""
+        result = extract_information(self.path_d+"/Mikolov.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
+
+    def Nasr(self):
+        titre = ""
+        Nom = ""
+        auteur = ""
+        abstract = ""
+        result = extract_information(self.path_d+"/Nasr.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
+
+    def Torres(self):
+        titre = ""
+        Nom = ""
+        auteur = ""
+        abstract = ""
+        result = extract_information(self.path_d+"/Torres.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
+
+    def Torres_moreno1998(self):
+        titre = ""
+        Nom = ""
+        auteur = ""
+        abstract = ""
+        result = extract_information(self.path_d+"/Torres-moreno1998.txt")
+
+        dict2 = {"Nom du fichier pdf": Nom, "Titre": titre, "Auteurs": auteur, "Abstract": abstract}
+        self.assertEqual(result, dict2)
