6323aa89860a5945fd49ba2a91ca29cdcf89a54a
Benoit <benoitmaurice56@protonmail.com>
2024-03-05 23:18:42 +0100

    Modification menu

--- src/menu.py
+++ src/menu.py
@@ -1,86 +1,83 @@
 import pytermgui as ptg
-import os
+from functools import partial
 
 
-def menuPdf(path: str) -> list:
-    OUTPUT = {}
+def menu_pdf(all_element_in_folder: list) -> list:
+    output = {}
 
-    def submit(manager: ptg.WindowManager, window: ptg.Window) -> None:
-        for widget in window:
+    def submit(manager_window: ptg.WindowManager, window_terminal: ptg.Window) -> None:
+        for widget in window_terminal:
             if isinstance(widget, ptg.InputField):
-                OUTPUT[widget.prompt] = widget.value
+                output[widget.prompt] = widget.value
                 continue
 
             if isinstance(widget, ptg.Container):
-                label, field = iter(widget)
-                OUTPUT[label.value] = field.value
+                label = widget[0]
+                if len(widget) > 1:
+                    field = widget[1]
+                    output[label.value] = field.value
+                else:
+                    output[label.value] = ""
 
-        manager.stop()
+        manager_window.stop()
 
     def window(liste):
-        CONFIG = """
-        config:
-            InputField:
-                styles:
-                    prompt: dim italic
-                    cursor: '@72'
-            Label:
-                styles:
-                    value: dim bold
+        config = """
+           config:
+               InputField:
+                   styles:
+                       prompt: dim italic
+                       cursor: '@72'
+               Label:
+                   styles:
+                       value: dim bold
 
-            Window:
-                styles:
-                    border: '60'
-                    corner: '60'
+               Window:
+                   styles:
+                       border: '60'
+                       corner: '60'
 
-            Container:
-                styles:
-                    border: '96'
-                    corner: '96'
-        """
+               Container:
+                   styles:
+                       border: '96'
+                       corner: '96'
+           """
 
         with ptg.YamlLoader() as loader:
-            loader.load(CONFIG)
+            loader.load(config)
 
         with ptg.WindowManager() as manager:
             input_fields = []
             for item in liste:
                 input_fields.append(ptg.InputField("", prompt=f"{item}: "))
 
-            window = (
-                ptg.Window(
-                    *input_fields,
-                    ["Submit", lambda *_: submit(manager, window)],
-                    width=60,
-                    box="DOUBLE",
-                )
-                .set_title("User Input")
-                .center()
+            window = ptg.Window(
+                ptg.Container(ptg.Label("Entrez une * \npour analyser le pdf"), ),
+                *input_fields,
+                ["Submit", lambda *_: submit(manager, window)],
+                width=60,
+                box="DOUBLE",
             )
 
             manager.add(window)
             manager.run()
 
-    # Example of usage
-
-    element_in_dir = os.listdir(path)
-
-    element_in_dir = [elt for elt in element_in_dir if len(elt) > 4 and elt[-4:] == ".pdf"]
+    # vérification que les éléments dans le dossier sont des pdf
+    element_in_dir = [elt for elt in all_element_in_folder if len(elt) > 4 and elt[-4:] == ".pdf"]
     element_in_dir.append("TOUS LES ELEMENTS ")
 
     window(element_in_dir)
 
     selected_pdfs = []
 
-    # Si "TOUS LES ELEMENTS : " a été sélectionné, ajouter tous les fichiers PDF
-    if "TOUS LES ELEMENTS : " in OUTPUT and OUTPUT["TOUS LES ELEMENTS : "] == "y":
-        selected_pdfs = [key.strip(': ') for key in OUTPUT.keys() if key != "TOUS LES ELEMENTS : "]
+    # Si "TOUS LES ELEMENTS : " a été sélectionné, ajout de tous les fichiers PDF
+    if output["TOUS LES ELEMENTS : "] != "":
+        selected_pdfs = element_in_dir[:-2]
 
-    # Sinon, ajouter les fichiers PDF correspondant aux réponses "y"
+    # Sinon, ajouter les fichiers PDF correspondant aux réponses
     else:
-        for key, value in OUTPUT.items():
-            if key != "TOUS LES ELEMENTS : " and value.lower() == "y":
+        for key, value in output.items():
+            if key != "TOUS LES ELEMENTS : " and value != "":
                 selected_pdfs.append(key.strip(': '))
 
     return selected_pdfs
-
