59e2b3d1d3dc3f2d441f98b24b54c8c08443b7d1
Benoit <benoitmaurice56@protonmail.com>
2024-03-14 08:32:48 +0100

    Modification test ajout exécution parser

--- Test/test.py
+++ Test/test.py
@@ -1,15 +1,16 @@
 from Test import extract
+import pathlib
 import sys
+import os
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        resAttendu_path = sys.argv[1]
-        folder_path = sys.argv[2]
+    path = str(pathlib.Path(__file__).parent.resolve())
+    path_dir = path[:path.rfind('/')]
 
-        comparer = extract.TextComparer(resAttendu_path)
-        comparer.compare_files(folder_path)
-    elif len(sys.argv) != 3:
-        print(f"Vous n'avez pas rentre le bon nombre d'argument, vous en avez rentre {len(sys.argv)-1} et non 2, voici le modele d'utilisation:")
-        print("test.py [/path/to/the/resAttendu.txt, /path/to/the/analyse_pdf/]")
+    os.system(f"python3 {path_dir}/main.py -t {path_dir}/Corpus_2022/ --all")
 
+    resAttendu_path = f"{path}/solution.txt"
+    folder_path = f"{path_dir}/Corpus_2022/analyse_pdf/"
 
+    comparer = extract.TextComparer(resAttendu_path)
+    comparer.compare_files(folder_path)
