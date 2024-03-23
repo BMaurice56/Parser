518fa452ad2d2c5d2beabf2036fd420bd5e3b3e6
benjamin <haurognebenjamin@gmail.com>
2024-03-23 14:26:47 +0100

    rename variable

--- Test/test.py
+++ Test/test.py
@@ -1,15 +1,18 @@
+import sys
+
 from Test import extract
 import pathlib
 import os
 
 if __name__ == "__main__":
-    path = str(pathlib.Path(__file__).parent.resolve())
-    path_dir = path[:path.rfind('/')]
+    path_test = str(pathlib.Path(__file__).parent.resolve())
+    path_paser = path_test[:path_test.rfind('/')]
 
-    os.system(f"python3 {path_dir}/main.py -t {path_dir}/Corpus_2022/ --all")
 
-    resAttendu_path = f"{path}/solutionTxt.txt"
-    folder_path = f"{path_dir}/Corpus_2022/analyse_pdf/"
+    os.system(f"python3 {path_paser}/main.py -t {path_paser}/Corpus_2022/ --all")
+
+    resAttendu_path = f"{path_test}/solutionTxt.txt"
+    folder_path = f"{path_paser}/Corpus_2022/analyse_pdf/"
 
     comparer = extract.TextComparer(resAttendu_path)
     comparer.compare_files(folder_path)
