0a6c740cfd801586954a11eb3240941769a619d1
benjamin <haurognebenjamin@gmail.com>
2024-03-23 14:28:00 +0100

    rename variable

--- Test/test.py
+++ Test/test.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     path_test = str(pathlib.Path(__file__).parent.resolve())
     path_paser = path_test[:path_test.rfind('/')]
 
-
     os.system(f"python3 {path_paser}/main.py -t {path_paser}/Corpus_2022/ --all")
 
     resAttendu_path = f"{path_test}/solutionTxt.txt"
