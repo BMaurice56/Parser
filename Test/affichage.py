ff159cf7a4fee5b16f2969a3616deb9eabbabb3a
arthur <arthurgros03@gmail.com>
2024-03-26 13:33:31 +0100

    paramètre d'entré : dictionnaire Barre total coresondant a la moyenne des séction.

--- Test/affichage.py
+++ Test/affichage.py
@@ -1,34 +1,57 @@
 from colorama import Fore, Style
 
+def selectcouleur(pourcentage : float):
+    """
 
-def afficher_barre_pourcentage(pourcentage: float) -> None:
+    :param pourcentage:
+    :return: la couleur relié au pourcentage
+    """
+    if pourcentage >= 90 :
+        return Fore.GREEN
+    elif pourcentage >=70:
+        return Fore.LIGHTYELLOW_EX
+    elif pourcentage >= 50:
+        return Fore.LIGHTMAGENTA_EX
+    else:
+        return Fore.RED
+
+
+def afficher_barre_pourcentage(pourcentages: dict) -> None:
     """
     Affiche la barre de pourcentage dans le terminal
 
-    :param pourcentage: valeur
+    :param pourcentages: list des pourcentages à afficher
     :return: None
     """
+    total = 0
+    max_longueur_nom = 10
 
-    # Assurez-vous que le pourcentage est dans la plage [0, 100]
-    if pourcentage < 0 or pourcentage > 100:
-        raise ValueError("Pourcentage impossible")
+    for el, p in pourcentages.items():
+        # Assurez-vous que le pourcentage est dans la plage [0, 100]
+        if p < 0 or p > 100:
+            raise ValueError("Pourcentage impossible")
+        total += p
+    total /= len(pourcentages)
 
-    pourcentage = max(0, min(100, int(pourcentage)))
+    print("Pourcentage par élément:")
+    for element, pourcentage in pourcentages.items():
+        couleur = selectcouleur(pourcentage)
+        barre = '■' * int(pourcentage / 5)  # Ajuste la division pour une meilleure échelle
+        espace = ' ' * (max_longueur_nom - len(element))  # Ajoute l'espace pour aligner les barres
+        print(f"{element}:{espace}\t {couleur}{barre}  {pourcentage}%{Style.RESET_ALL}")
 
+    print()
     # Convertis le pourcentage en un nombre entier pour l'affichage de la barre
-    pourcentage_entier = int(pourcentage)
+    pourcentage_entier = int(total)
 
     # Choisi la couleur en fonction du pourcentage
-    if pourcentage >= 95:
-        couleur = Fore.GREEN
-    elif pourcentage >= 85:
-        couleur = Fore.LIGHTYELLOW_EX
-    elif pourcentage >= 70:
-        couleur = Fore.YELLOW
-    else:
-        couleur = Fore.RED
+    couleur = selectcouleur(total)
 
     # Créer et affiche la barre de pourcentage avec le caractère carré coloré
     barre_texte = "Pourcentage de ressemblance avec le résultat attendu: "
-    barre_texte += f'{round(pourcentage, 2)}% \n |{"■" * pourcentage_entier}{"_" * (100 - pourcentage_entier)}|'
+    barre_texte += f'{round(total, 2)}% \n |{"■" * pourcentage_entier}{"_" * (100 - pourcentage_entier)}|'
     print(f"{couleur}{barre_texte} {Style.RESET_ALL}")
+
+
+
+afficher_barre_pourcentage({"Intro": 51, "Corps": 10, "conclusion": 100})
