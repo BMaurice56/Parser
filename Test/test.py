7bfad4676895e3c71bcebd47a798a277cd0658eb
Benoit <benoitmaurice56@protonmail.com>
2024-04-08 21:31:38 +0200

    Suppression test sur txt dans fichier test et test_old

--- Test/test.py
+++ Test/test.py
@@ -1,30 +1,16 @@
 from extract import TextComparer
 import pathlib
-import sys
 import os
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        raise ValueError("Erreur nombre argument")
-
-    argv = sys.argv[1]
-    if argv != "-t" and argv != "-x":
-        raise ValueError("Erreur argument rentré")
-
     # Chemin du dossier
     path_test = str(pathlib.Path(__file__).parent.resolve())
     path_paser = path_test[:path_test.rfind('/')]
 
-    res_attendu_path = f"{path_test}/solution"
+    res_attendu_path = f"{path_test}/solution2023.xml"
     folder_path = f"{path_paser}/Corpus_2023_FINAL/analyse_pdf/"
 
-    if argv == "-t":
-        res_attendu_path += "Txt.txt"
+    os.system(f"python3.10 {path_paser}/main.py -x {path_paser}/Corpus_2023_FINAL/ --all")
 
-    elif argv == "-x":
-        res_attendu_path += "Xml.xml"
-
-    os.system(f"python3.10 {path_paser}/main.py {argv} {path_paser}/Corpus_2023_FINAL/ --all")
-
-    comparer = TextComparer(res_attendu_path, argv)
+    comparer = TextComparer(res_attendu_path, "-x")
     comparer.compare_files(folder_path)
